88f098bc76738e6b50c2479295d133b2445eb770
chrsrck <chrsrck292@gmail.com>
2015-06-16 12:52:12 -0400

    Fourth stage complete

---
+++ app/src/main/java/com/example/chrsrck/stormy/CurrentWeather.java
@@ -0,0 +1,116 @@
+package com.example.chrsrck.stormy;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.TimeZone;
+
+/**
+ * Created by chrsrck on 6/15/15.
+ */
+public class CurrentWeather {
+    private String mIcon;
+    private long mTime;
+    private double mTemperature;
+    private double mHimidity;
+    private double mPrecipChance;
+    private String mSummary;
+    private String mTimezone;
+
+    public String getTimezone() {
+        return mTimezone;
+    }
+
+    public void setTimezone(String timezone) {
+        mTimezone = timezone;
+    }
+
+    public String getIcon() {
+        int iconId = R.mipmap.clear_day;
+
+        if (mIcon.equals("clear-day")) {
+            iconId = R.mipmap.clear_day;
+        }
+        else if (mIcon.equals("clear-night")) {
+            iconId = R.mipmap.clear_night;
+        }
+        else if (mIcon.equals("rain")) {
+            iconId = R.mipmap.rain;
+        }
+        else if (mIcon.equals("snow")) {
+            iconId = R.mipmap.snow;
+        }
+        else if (mIcon.equals("sleet")) {
+            iconId = R.mipmap.sleet;
+        }
+        else if (mIcon.equals("wind")) {
+            iconId = R.mipmap.wind;
+        }
+        else if (mIcon.equals("fog")) {
+            iconId = R.mipmap.fog;
+        }
+        else if (mIcon.equals("cloudy")) {
+            iconId = R.mipmap.cloudy;
+        }
+        else if (mIcon.equals("partly-cloudy-day")) {
+            iconId = R.mipmap.partly_cloudy;
+        }
+        else if (mIcon.equals("partly-cloudy-night")) {
+            iconId = R.mipmap.cloudy_night;
+        }
+
+        return mIcon;
+    }
+
+    public void setIcon(String icon) {
+        mIcon = icon;
+    }
+
+    public long getTime() {
+        return mTime;
+    }
+
+    public String getFormattedTime(){
+        SimpleDateFormat formatter = new SimpleDateFormat("h:mm a");
+        formatter.setTimeZone(TimeZone.getTimeZone(getTimezone()));
+        Date dateTime = new Date(getTime() *1000);
+        String timeString = formatter.format(dateTime);
+        return timeString;
+    }
+
+    public void setTime(long time) {
+        mTime = time;
+    }
+
+    public double getTemperature() {
+        return mTemperature;
+    }
+
+    public void setTemperature(double temperature) {
+        mTemperature = temperature;
+    }
+
+    public double getHimidity() {
+        return mHimidity;
+    }
+
+    public void setHimidity(double himidity) {
+        mHimidity = himidity;
+    }
+
+    public double getPrecipChance() {
+        return mPrecipChance;
+    }
+
+    public void setPrecipChance(double precipChance) {
+        mPrecipChance = precipChance;
+    }
+
+    public String getSummary() {
+        return mSummary;
+    }
+
+    public void setSummary(String summary) {
+        mSummary = summary;
+    }
+
+}
--- app/src/main/java/com/example/chrsrck/stormy/MainActivity.java
+++ app/src/main/java/com/example/chrsrck/stormy/MainActivity.java
@@ -18,11 +18,15 @@ import com.squareup.okhttp.OkHttpClient;
 import com.squareup.okhttp.Request;
 import com.squareup.okhttp.Response;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.io.IOException;
 
 
 public class MainActivity extends Activity {
     public static final String TAG = MainActivity.class.getSimpleName();
+    private CurrentWeather mCurrentWeather;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -50,14 +54,18 @@ public class MainActivity extends Activity {
                 @Override
                 public void onResponse(Response response) throws IOException {
                     try {
-                        Log.v(TAG, response.body().string());
-
+                        String jsonData = response.body().string();
+                        Log.v(TAG, jsonData);
                         if (response.isSuccessful()) {
-
+                            mCurrentWeather = getCurrentDetails(jsonData);
                         } else {
                             alertUserAboutError();
                         }
-                    } catch (IOException e) {
+                    }
+                    catch (IOException e) {
+                        Log.e(TAG, "Exception caught: ", e);
+                    }
+                    catch (JSONException e){
                         Log.e(TAG, "Exception caught: ", e);
                     }
                 }
@@ -69,6 +77,24 @@ public class MainActivity extends Activity {
         }
 
         Log.d(TAG, "Main UI code is running!");
+    }
+
+    private CurrentWeather getCurrentDetails(String jsonData) throws JSONException {
+        JSONObject forecast = new JSONObject(jsonData);
+        String timezone = forecast.getString("timezone");
+        JSONObject currently = forecast.getJSONObject("currently");
+
+        CurrentWeather currentWeather = new CurrentWeather();
+        currentWeather.setHimidity(currently.getDouble(("humidity")));
+        currentWeather.setTime(currently.getLong("time"));
+        currentWeather.setIcon(currently.getString("icon"));
+        currentWeather.setPrecipChance(currently.getDouble("precipProbability"));
+        currentWeather.setSummary(currently.getString("summary"));
+        currentWeather.setTemperature(currently.getDouble("temperature"));
+        currentWeather.setTimezone(timezone);
+
+        Log.d(TAG, currentWeather.getFormattedTime());
+        return currentWeather;
     }
 
     private boolean isNetworkAvailible() {
